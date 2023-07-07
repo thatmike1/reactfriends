7e49ecd9d3ea35aff88efd85aad4377fc2b396bf
thatmike1 <misa.psencik@gmail.com>
2023-07-07 19:57:28 +0200

    styling and font

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
       <input
         className="pa3 br3 ba b--blue bg-lightest-blue"
         type="search"
-        placeholder="search robots"
+        placeholder="Search robots..."
         onChange={searchChange}
       />
     </div>
